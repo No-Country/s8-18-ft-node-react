7037de8e12348f1551f8ba53dac273b846e44cad
gv <gonzalovasquezher@gmail.com>
2023-05-19 16:46:14 -0300

    husky with eslint staged precommit

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 export const Home: FC = () => {
   return (
     <div>
-      <h1>InventoryApp</h1>
+      <h1 className='border-green-500'>InventoryApp</h1>
     </div>
   );
 };
