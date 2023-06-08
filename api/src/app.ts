69b4fb06c94b0e8adf09ab3b870f8c0c34d0dea0
gv <gonzalovasquezher@gmail.com>
2023-06-08 10:26:39 -0300

    cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 import authConfig from './auth/passport'
 import routes from './routes'
+
 import errorHandlerMiddleware from './middlewares/errorHandler.middleware'
 import { authMiddleware } from './middlewares/auth.middleware'
 
@@ -11,6 +13,16 @@ const API_URL = process.env.API_URL || '/api/1.0'
 
 const app = express()
 authConfig()
+
+const CLIENT_URL = process.env.CLIENT_URL
+
+if (CLIENT_URL) {
+  app.use(
+    cors({
+      origin: CLIENT_URL,
+    }),
+  )
+}
 
 app.use(express.json())
 app.use(cookieParser())
